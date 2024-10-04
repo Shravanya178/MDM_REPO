7fa7156cab36056da083594b4fc60b9f11138991
Shravanya178 <2023.shravanya.andhale@ves.ac.in>
2024-10-04 12:33:30 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -7,16 +7,7 @@ public static boolean ispalindrome(String str){
 }
 
 public static void main(String args[]){
-  Scanner sc= new Scanner(System.in);
+
   System.out.println("Enter a string: ");
-  String str= sc.nextLine();
 
-  if (ispalindrome(str)) {
-    System.out.println(str + " is a palindrome.");
-} else {
-    System.out.println(str + " is not a palindrome.");
-}
-
-sc.close();
-}
 }
